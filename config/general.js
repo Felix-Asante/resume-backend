68cf5fd8e2e36e0e6e00679a2ec145e6b06119e2
Felix-Asante <quamzgraphix@gmail.com>
2022-05-27 08:49:40 +0100

    v1

--- config/general.js
+++ config/general.js
@@ -1,16 +1,5 @@
 export const options = {
-	format: "A3",
+	format: "A4",
 	orientation: "portrait",
 	border: "8mm",
-
-	footer: {
-		height: "28mm",
-		// contents: {
-		// 	first: "Cover page",
-		// 	2: "Second page", // Any page number is working. 1-based index
-		// 	default:
-		// 		'<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
-		// 	last: "Last Page",
-		// },
-	},
 };
--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import pdf from "pdf-creator-node";
 import fs from "fs";
 import cors from "cors";
 import { options } from "./config/general.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import pdfToBase64 from "pdf-to-base64";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -13,20 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 var html = fs.readFileSync("template.html", "utf8");
-var users = [
-	{
-		name: "Shyam",
-		age: "26",
-	},
-	{
-		name: "Navjot",
-		age: "26",
-	},
-	{
-		name: "Vitthal",
-		age: "26",
-	},
-];
 
 app.post("/create-pdf", (req, res) => {
 	const profile = [];
@@ -37,16 +29,16 @@ app.post("/create-pdf", (req, res) => {
 		html: html,
 		data: {
 			profile: profile,
-			projectTitle: project.projectTitle,
-			projects: project.projects,
-			awardTitle: award.awardTitle,
-			awards: award.awards,
-			educations: education.content,
-			skillTitle: skill.skillTitle,
-			skills: skill.competence,
-			workTitle: work.workTitle,
-			works: work.works,
-			educationTitle: req.body.education.educationTitle,
+			projectTitle: project?.projectTitle,
+			projects: project?.projects,
+			awardTitle: award?.awardTitle,
+			awards: award?.awards,
+			educations: education?.content,
+			skillTitle: skill?.skillTitle,
+			skills: skill?.competence,
+			workTitle: work?.workTitle,
+			works: work?.works,
+			educationTitle: education?.educationTitle,
 		},
 		path: "./output.pdf",
 		type: "",
@@ -64,6 +56,19 @@ app.post("/create-pdf", (req, res) => {
 	res.status(200).json({ data: req.body });
 });
 
+app.get("/fetch-pdf", (req, res) => {
+	var data = fs.readFileSync(`${__dirname}/output.pdf`);
+	res.contentType("application/pdf");
+	pdfToBase64(`${__dirname}/output.pdf`)
+		.then((response) => {
+			res.json({ data: response });
+		})
+		.catch((error) => {
+			console.log(error); //Exepection error....
+		});
+
+	// res.sendFile(__dirname + "/output.pdf");
+});
 app.listen(PORT, () => {
 	console.log(`SERVER RUNNING ON PORT ${PORT}`);
 });
